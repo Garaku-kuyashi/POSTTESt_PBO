654245a9175f1df272f7902e6e3e95f387292877
M. Fahrianor <moraxz112@gmail.com>
2026-04-05 18:46:38 +0800

    postest 3

---
+++ POSSTEST-PBO/POSTTEST-3/src/Operator.java
@@ -0,0 +1,36 @@
+public class Operator {
+    protected String nama;
+    protected int rarity;
+
+    public Operator(String nama, int rarity){
+        this.nama = nama;
+        this.rarity = rarity;
+    }
+
+    public void dataTampil(){
+        System.out.println("Nama   : " + nama);
+        System.out.println("Rarity   : " + rarity);
+    }
+
+
+//    setter
+
+    void setNama(String nama){
+
+        this.nama = nama;
+    }
+
+    void setRarity(int rarity){
+        this.rarity = rarity;
+    }
+
+//    getter
+
+    public String getNama() {
+        return this.nama;
+    }
+
+    public int getRarity() {
+        return this.rarity;
+    }
+}
